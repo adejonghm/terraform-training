dc6e18915ab9471072769b472d0f84434869480b
Alejandro de Jongh <dejongh.morell@gmail.com>
2022-07-16 03:00:16 -0300

    upd: provider file

--- aws-get-started/provider.tf
+++ aws-get-started/provider.tf
@@ -7,12 +7,12 @@
 */
 
 terraform {
-  # cloud {
-  #   organization = "hc-training"
-  #   workspaces {
-  #     name = "aws-get-started"
-  #   }
-  # }
+  cloud {
+    organization = "hc-training"
+    workspaces {
+      name = "aws-get-started"
+    }
+  }
 
   required_providers {
     aws = {
@@ -26,6 +26,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAREHHM4R7DYIVZVGU"
-  secret_key = "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"
+  access_key = "AKIAWTSWFV2CUP7K2SPV"
+  secret_key = "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"
 }
