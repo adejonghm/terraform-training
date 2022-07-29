06f866de6f62065998d4ea001e1455331a7c39a7
Alejandro de Jongh <dejongh.morell@gmail.com>
2022-07-29 18:45:55 -0300

    upd: a subnet was added

--- oci-get-started/oci_vcn.tf
+++ oci-get-started/oci_vcn.tf
@@ -4,3 +4,12 @@ resource "oci_core_vcn" "internal" {
   compartment_id = "ocid1.compartment.oc1..aaaaaaaahntpkll3v3kpxcdtprybokgj6wxfqcu7lqygcnv4y6owkkcm6idq"
   display_name   = "My internal VCN"
 }
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "172.16.0.0/24"
+  compartment_id             = "ocid1.compartment.oc1..aaaaaaaahntpkll3v3kpxcdtprybokgj6wxfqcu7lqygcnv4y6owkkcm6idq"
+  display_name               = "Dev subnet"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
+}
