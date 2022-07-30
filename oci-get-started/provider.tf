93a3b979ae2f0b6963b9511486fbac9a5a324f3e
Alejandro de Jongh <dejongh.morell@gmail.com>
2022-07-30 00:46:15 -0300

    upd: new configurations

--- oci-get-started/provider.tf
+++ oci-get-started/provider.tf
@@ -1,19 +1,22 @@
 /*
   Made by Alejandro de Jongh
 
-  Provider definition
+  Provider definition.
 */
 
 terraform {
   required_providers {
     oci = {
-      source = "oracle/oci"
+      source  = "oracle/oci"
+      version = "~> 4.86.0"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "oci" {
-  region              = var.region
   auth                = "SecurityToken"
   config_file_profile = "learn-terraform"
+  region              = var.region
 }
