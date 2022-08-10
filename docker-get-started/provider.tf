69f749d5fe6f49db844abdf972b1fb0c1e023106
Alejandro de Jongh <dejongh.morell@gmail.com>
2022-08-10 02:22:52 -0300

    add: Get Started Docker

---
+++ docker-get-started/provider.tf
@@ -0,0 +1,16 @@
+/*
+  Made by Alejandro de Jongh
+
+  Provider definition.
+*/
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
---
+++ docker-get-started/resources.tf
@@ -0,0 +1,19 @@
+/*
+  Made by Alejandro de Jongh
+
+  Providing a Container with Nginx with Docker.
+*/
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = var.container_name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
