56c97da7914120770065629758d1c84cc471291b
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-08-11 00:24:29 -0300

    fix: added new tag

--- aws-get-started/aws_instance.tf
+++ aws-get-started/aws_instance.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
+    "Name"             = var.instance_name,
     "Group"            = "HashiCorpTraining",
     "Operating System" = "GNU/Linux"
   }
