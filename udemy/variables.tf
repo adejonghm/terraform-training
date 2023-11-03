264c232479cb6e816895303a1514065ecbbd4b09
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-11-03 10:55:22 -0300

    fix: Chaged the variable type

--- udemy/variables.tf
+++ udemy/variables.tf
@@ -2,13 +2,15 @@
 Developed by adejonghm
 ----------
 
-November 1, 2o23
+November 1, 2023
 */
 
 variable "AWS_ACCESS_KEY_ID" {
+  type = string
   description = "Access Key environment variable for AWS Provider"
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
   description = "Secret Access Key environment variable for AWS provider"
 }
