b7ceb1f8a32cbd2f787cedf726d388f181a81bef
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-11-10 00:00:34 -0300

    fix: Using environment variables for AWS provider

--- udemy/main.tf
+++ udemy/main.tf
@@ -23,7 +23,8 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region                   = var.region
+  shared_credentials_files = ["$HOME/.aws/credentials"]
 
   default_tags {
     tags = {
--- udemy/variables.tf
+++ udemy/variables.tf
@@ -6,7 +6,7 @@ November 1, 2023
 */
 
 variable "region" {
-    type = string
-    default = "us-west-1"
-    description = "AWS Region that I'm using"
+  type        = string
+  default     = "us-west-1"
+  description = "AWS Region that I'm using"
 }
