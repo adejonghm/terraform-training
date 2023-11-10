a80d15037a58d18e9cdc5ed3af7f6bc005074c7a
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-11-10 19:48:02 -0300

    feat: Added new Azure variables

---
+++ udemy/azure/locals.tf
@@ -0,0 +1,13 @@
+/*
+Developed by adejonghm
+----------
+
+November 10, 2023
+*/
+
+locals {
+  commong_tags = {
+    owner      = "adejonghm"
+    managed-by = "terraform"
+  }
+}
--- udemy/azure/tfstate-storage_account.tf
+++ udemy/azure/tfstate-storage_account.tf
@@ -7,9 +7,9 @@ November 10, 2023
 
 resource "azurerm_resource_group" "terraform-rs-group" {
   name     = "terraform-rs-group"
-  location = ""
+  location = var.location
 
-  tags = {}
+  tags = local.commong_tags
 }
 
 
@@ -17,11 +17,11 @@ resource "azurerm_storage_account" "az_storage-tfstate-files" {
   name                     = "ud30183408-state-files"
   resource_group_name      = azurerm_resource_group.terraform-rs-group.name
   location                 = azurerm_resource_group.terraform-rs-group.location
-  account_tier             = ""
-  account_replication_type = "GRS"
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
   depends_on               = [azurerm_resource_group.terraform-rs-group]
 
-  tags = ""
+  tags = local.commong_tags
 }
 
 resource "azurerm_storage_container" "tfstate-files" {
