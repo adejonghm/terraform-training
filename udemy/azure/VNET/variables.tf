ff1db7ae75c1ca92001cb0f8aad73a5a4a38b6e1
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-12-01 23:58:10 -0300

    feat: Added Azure VNet

---
+++ udemy/azure/VNET/variables.tf
@@ -0,0 +1,12 @@
+/*
+Developed by adejonghm
+----------
+
+November 29, 2023
+*/
+
+variable "az_location" {
+  type        = string
+  default     = "East US"
+  description = "Azure location that I'm using"
+}
