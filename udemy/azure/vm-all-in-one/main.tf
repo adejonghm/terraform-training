b4a3543f43c71ac32853725949a2bfed72b4e34e
adejonghm <dejongh.morell@gmail.com>
2024-03-01 00:18:08 -0300

    feat: Creating a Linux VM with one file

---
+++ udemy/azure/vm-all-in-one/main.tf
@@ -0,0 +1,27 @@
+/*
+Developed by adejonghm
+----------
+
+Feb 29, 2024
+*/
+
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "RgUdTfstate"
+    storage_account_name = "saudtfstate"
+    container_name       = "remote-state"
+    key                  = "udemo-vm-all-in/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
