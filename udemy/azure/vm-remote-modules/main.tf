b3f66ab9cffa1c67406154171f1f221e1f53f518
adejonghm <dejongh.morell@gmail.com>
2024-03-04 16:41:23 -0300

    fix: Refactoring code in VM Remote Module

--- udemy/azure/vm-remote-modules/main.tf
+++ udemy/azure/vm-remote-modules/main.tf
@@ -6,13 +6,27 @@ March 4, 2024
 */
 
 terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    key                  = "udemo-vm-remote-module/terraform.tfstate"
+    container_name       = "remote-state"
+    resource_group_name  = "RgUdTfstate"
+    storage_account_name = "saudtfstate"
+  }
 }
 
-provider "" {
-
+provider "azurerm" {
+  features {}
 }
 
-module "name" {
+# module "name" {
 
-}
+# }
--- udemy/azure/vm-remote-modules/variables.tf
+++ udemy/azure/vm-remote-modules/variables.tf
@@ -2,95 +2,95 @@
 Developed by adejonghm
 ----------
 
-March 2, 2024
+March 4, 2024
 */
 
 variable "location" {
   description = "Azure Region used in the Terraform training"
+  default     = "East US 2"
   type        = string
-  # default     = "East US 2"
 }
 
 variable "rg-name" {
   description = "Resource group used to create an all-in-one Virtual Machine"
+  default     = "RgVmRemoteModule"
   type        = string
-  # default     = "RgVmRemoteModule"
 }
 
 variable "net-interface-name" {
   description = "Network Interface used to create an all-in-one Virtual Machine"
+  default     = "NiVmRemoteModule"
   type        = string
-  # default     = "NiVmRemoteModule"
 }
 
 variable "public-ip-name" {
   description = "Public IP for the all-in-one Virtual Machine"
+  default     = "PublicIpVmRemoteModule"
   type        = string
-  # default     = "PublicIpVmRemoteModule"
 }
 
 variable "vm-name" {
   description = "Name of the All-in-One Virtual Machine"
+  default     = "VmRemoteModule"
   type        = string
-  # default     = "VmRemoteModule"
 }
 
 variable "vm-size" {
   description = "Type and size of the All-in-one Virtual Machine"
+  default     = "Standard_B1s"
   type        = string
-  # default     = "Standard_B1s"
 }
 
 variable "vm-username" {
   description = "Username user in the All-in-one Virtual Machine"
+  default     = "terraform"
   type        = string
-  # default     = "terraform"
 }
 
 variable "vm-disk-type" {
   description = "Storage Account disk type used in the Virtual Machine"
+  default     = "Standard_LRS"
   type        = string
-  # default     = "Standard_LRS"
 }
 
 variable "vm-disk-cache" {
   description = "Disk cache type used in the Virtual Machine"
+  default     = "ReadWrite"
   type        = string
-  # default     = "ReadWrite"
 }
 
 variable "vm-image-sku" {
   description = "Sku image reference used to create a Virtual Machine"
-  # default     = "22_04-lts"
-  type = string
+  default     = "22_04-lts"
+  type        = string
 }
 
 variable "vm-image-offer" {
   description = "Sku image reference used to create a Virtual Machine"
+  default     = "0001-com-ubuntu-server-jammy"
   type        = string
-  # default     = "0001-com-ubuntu-server-jammy"
 }
 
 variable "vm-image-version" {
   description = "Sku image reference used to create a Virtual Machine"
   type        = string
-  # default     = "latest"
+  default     = "latest"
 }
 
 variable "vm-image-publisher" {
   description = "Sku image reference used to create a Virtual Machine"
+  default     = "Canonical"
   type        = string
-  # default     = "Canonical"
 }
 
 variable "private-ip-allocation-method" {
   description = "The private IP allocation method used to create a Virtual Machine"
+  default     = "Dynamic"
   type        = string
-  # default     = "Dynamic"
 }
 
 variable "public-ip-allocation-method" {
   description = "The public IP allocation method used to create a Virtual Machine"
+  default     = "Dynamic"
   type        = string
-  # default     = "Dynamic"
 }
