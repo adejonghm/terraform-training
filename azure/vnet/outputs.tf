a0270a552e81a048556e022dd988a64c23f831b4
adejonghm <dejongh.morell@gmail.com>
2025-07-11 00:03:51 -0300

    fix: Added new outputs for Data rerouces

--- azure/vnet/outputs.tf
+++ azure/vnet/outputs.tf
@@ -17,3 +17,13 @@ output "subnets-id" {
   }
   description = "Subnet ID created in the Virtual Network"
 }
+
+output "vnet-name" {
+  value       = azurerm_virtual_network.vnet.name
+  description = "The name of the Virtual Network"
+}
+
+output "vnet-rg" {
+  value       = azurerm_virtual_network.vnet.resource_group_name
+  description = "The resource group where the Virtual Network is hosted"
+}
