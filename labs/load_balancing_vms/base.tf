c4b61931825e776b71e65f4f6a791d0211695ac8
adejonghm <dejongh.morell@gmail.com>
2025-08-01 00:27:16 -0300

    core: Called the WinVM module

--- labs/load_balancing_vms/base.tf
+++ labs/load_balancing_vms/base.tf
@@ -4,6 +4,9 @@ Developed by adejonghm
 
 July 9, 2025
 
+NOTES:
+- Add two Linux VMs, one in each subnet. Use for_each or dynamic
+
 */
 
 
@@ -41,27 +44,62 @@ resource "azurerm_resource_group" "rg" {
   tags = module.finops.tags
 }
 
-# CREATING THE VM
+module "vm_windows" {
+  source = "./modules/vm_windows"
+
+  ## NETWORK VARIABLES
+  subnet_id = data.terraform_remote_state.subnets.outputs.subnets_id[0]
+
+  ## VM VARIABLES
+  location = azurerm_resource_group.rg.location
+  rg_name  = azurerm_resource_group.rg.name
+  vm_name  = "vmloadbalancerwn01"
+  vm_user  = "wn-user01"
+  vm_pass  = var.win_pass
+
+  ## OS VARIABLES
+  os_publisher = "MicrosoftWindowsServer"
+  os_offer     = "WindowsServer"
+  os_sku       = "2022-datacenter-g2"
+  os_version   = "latest"
+
+  ## OPTIONAL VARIABLES
+  # pip_allocation_method     = ""
+  # nic_ip_config_name        = ""
+  # nic_private_ip_allocation = ""
+  # vm_size                   = ""
+  # os_disk_cache             = ""
+  # os_disk_type              = ""
+
+  tags = module.finops.tags
+}
+
 module "vm_ubuntu" {
   source = "./modules/vm_linux"
 
-  # NETWORK VARIABLES
-  pip_name                   = "vmlx001-pip"
-  ip_config_name             = "vmlx001_ip_configs"
-  subnet_id                  = data.terraform_remote_state.subnets.outputs.subnets_id[0]
-  private_ip_allocation_mode = "Dynamic"
-  pip_allocation_mode        = "Static"
+  ## NETWORK VARIABLES
+  subnet_id = data.terraform_remote_state.subnets.outputs.subnets_id[1]
 
-  # VM VARIABLES
-  rg_name         = azurerm_resource_group.rg.name
-  location        = azurerm_resource_group.rg.location
-  ssh_public_key  = file("~/.ssh/id_rsa.pub")
-  vm_name         = "vmlblx01"
-  vm_size         = "Standard_B1s"
-  vm_user         = "lx-user01"
-  vm_os_disk_type = "Standard_LRS"
-  vm_os_sku       = "22_04-lts"
-  vm_os_offer     = "0001-com-ubuntu-server-jammy"
+  ## VM VARIABLES
+  rg_name        = azurerm_resource_group.rg.name
+  location       = azurerm_resource_group.rg.location
+  ssh_public_key = file("~/.ssh/id_rsa.pub")
+  vm_name        = "vmloadbalancerlx01"
+  vm_user        = "lx-user01"
+
+  ## OS VARIABLES
+  os_publisher = "Canonical"
+  os_offer     = "0001-com-ubuntu-server-jammy"
+  os_sku       = "22_04-lts"
+  os_version   = "latest"
+
+  ## OPTIONAL VARIABLES
+  # pip_allocation_method     = ""
+  # nic_ip_config_name        = ""
+  # nic_private_ip_allocation = ""
+  # vm_size                   = ""
+  # os_disk_cache             = ""
+  # os_disk_type              = ""
 
   tags = module.finops.tags
 }
