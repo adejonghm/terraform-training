7305ca58892e0075fd66456d3d1c72ae71658c71
adejonghm <dejongh.morell@gmail.com>
2025-09-06 17:16:10 -0300

    fix: Changed the Nginx default port

--- azure/labs/load_balancer_servers/scripts/install_nginx.sh
+++ azure/labs/load_balancer_servers/scripts/install_nginx.sh
@@ -6,6 +6,10 @@ apt-get update -y
 # install nginx
 apt-get install -y nginx
 
+# change nginx to listen on port 8090 instead of 80
+sed -i 's/listen 80 default_server;/listen 8090 default_server;/g' /etc/nginx/sites-available/default
+sed -i 's/listen \[::\]:80 default_server;/listen [::]:8090 default_server;/g' /etc/nginx/sites-available/default
+
 # remove index.nginx-debian.html
 rm -f /var/www/html/index.nginx-debian.html
 
