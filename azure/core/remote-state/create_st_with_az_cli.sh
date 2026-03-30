9e117c1194e55c1938e2399dde70ef0630a6069a
adejonghm <dejongh.morell@gmail.com>
2026-03-30 16:22:21 -0300

    core: Created branches for each provider

--- azure/core/remote-state/create_st_with_az_cli.sh
+++
@@ -1,5 +0,0 @@
-# Creat a resource group
-az group create --name rgukscstbaseresrcs --location uksouth
-
-# Create a storage account 
-az storage account create --location uksouth --resource-group rgukscstbaseresrcs --name stukscstterrfstate --sku Standard_LRS --min-tls-version TLS1_2
--- azure/udm/vm-local-module/outputs.tf
+++
@@ -1,9 +0,0 @@
-/*
-Developed by adejonghm
-----------
-
-March 5, 2024
-*/
-output "public-ip" {
-  value = azurerm_linux_virtual_machine.vm-local-mod.public_ip_address
-}
